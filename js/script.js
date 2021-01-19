73d864d48c1abf2e9ee69b370d72d4e087442307
Yoad-Duani <68271120+Yoad-Duani@users.noreply.github.com>
2021-01-19 23:40:03 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -170,30 +170,18 @@ setTimeout(function(){
 
 
 
-$(document).ready(function() {
-   var screenHeight = $(window).height();
-   $('div-with-background-image').css('height', screenHeight + 'px');
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
-  
-  
-  
-//   $(function(){
 
-//   var $w = $(window),
-//       $header = $('.header');
-
-//   // Fix background image jump on mobile
-//   if ((/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
-//     $header.css({'top': 'auto', 'bottom': 0});
-
-//     $w.resize(sizeBackground);
-//     sizeBackground();
-//   }
-
-//   function sizeBackground() {
-//      $header.height(screen.height);
-//   }
-// });
 
 
 
